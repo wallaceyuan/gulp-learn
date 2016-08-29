7db346deaf6ab5da3ff1e473dae8580b039d01d7
wallaceyuan <1272864289@qq.com>
2016-08-29 17:57:15 +0800

    reload

---
+++ app/js/1.tmp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by yuan on 2016/8/29.
+ */
+console.log('1.tmp.js')
---
+++ dist/js/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by yuan on 2016/8/29.
+ */
+console.log('1.tmp.js')
+/**
+ * Created by yuan on 2016/8/29.
+ */
+console.log('index.js')
+/**
+ * Created by Yuan on 2016/8/23.
+ */
+console.log('src');
